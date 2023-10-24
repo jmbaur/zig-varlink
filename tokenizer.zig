8bd791282edafadda54f8d4820b24b394fd6affb
Väinö Mäkelä <vaino.makela@iki.fi>
2023-10-24 19:50:38 +0300

    tokenizer: Add simple error location tracking
    All methods that can error try to mark an error position if possible.
    This will make the final product more usable without making the code too
    complex.

--- tokenizer.zig
+++ tokenizer.zig
@@ -63,45 +63,85 @@ test skipAllWhitespace {
 }
 
 /// Skip all whitespace and comments until EOL. Returns error.ExpectedEol if an
-/// EOL was not found.
-fn skipEol(input: []const u8) error{ExpectedEol}![]const u8 {
+/// EOL was not found and sets error_pos.
+fn skipEol(input: []const u8, error_pos: *?[*]const u8) error{ExpectedEol}![]const u8 {
     const result = skipLineWhitespace(input);
     if (result[0]) {
         return result[1];
     } else {
+        if (result[1].len > 0) {
+            error_pos.* = result[1].ptr;
+        }
         return error.ExpectedEol;
     }
 }
 
 test skipEol {
-    try testing.expectEqualStrings("a", try skipEol("\na"));
-    try testing.expectEqualStrings(" \na", try skipEol(" \t # test\n \na"));
-    try testing.expectError(error.ExpectedEol, skipEol("a\n"));
+    var error_pos: ?[*]const u8 = null;
+    try testing.expectEqualStrings("a", try skipEol("\na", &error_pos));
+    try testing.expectEqualStrings(
+        " \na",
+        try skipEol(" \t # test\n \na", &error_pos),
+    );
+    const testString = "a\n";
+    try testing.expectError(error.ExpectedEol, skipEol(testString, &error_pos));
+    try testing.expectEqual(testString.ptr, error_pos.?);
 }
 
 /// Skip the given word from the start of input and return the rest of the
-/// input. Returns error.ExpectedWord if the input doesn't start with the word.
-fn skipWord(input: []const u8, word: []const u8) error{ExpectedWord}![]const u8 {
+/// input. Returns error.ExpectedWord if the input doesn't start with the word
+/// and sets error_pos.
+fn skipWord(
+    input: []const u8,
+    word: []const u8,
+    error_pos: *?[*]const u8,
+) error{ExpectedWord}![]const u8 {
     if (!std.mem.startsWith(u8, input, word)) {
+        if (input.len > 0) {
+            error_pos.* = input.ptr;
+        }
         return error.ExpectedWord;
     }
     return input[word.len..];
 }
 
 test skipWord {
-    try testing.expectEqualStrings("a", try skipWord("testa", "test"));
-    try testing.expectError(error.ExpectedWord, skipWord("test", "a"));
+    var error_pos: ?[*]const u8 = null;
+    try testing.expectEqualStrings(
+        "a",
+        try skipWord(
+            "testa",
+            "test",
+            &error_pos,
+        ),
+    );
+    const testString = "test";
+    try testing.expectError(
+        error.ExpectedWord,
+        skipWord(
+            testString,
+            "a",
+            &error_pos,
+        ),
+    );
+    try testing.expectEqual(testString.ptr, error_pos.?);
 }
 
 /// Tokenize a Varlink name into a .name token and return the rest of the input.
 /// This allows a much more liberal use of characters than the spec. Returns
-/// error.InvalidName if the input doesn't start with a valid name.
-fn tokenizeName(input: []const u8, tokens: *Tokens) ![]const u8 {
+/// error.InvalidName and sets error_pos.* if the input doesn't start
+/// with a valid name.
+fn tokenizeName(
+    input: []const u8,
+    tokens: *Tokens,
+    error_pos: *?[*]const u8,
+) ![]const u8 {
     for (input, 0..) |c, i| {
         switch (c) {
             '0'...'9', 'A'...'Z', 'a'...'z', '_', '-', '.' => {},
             else => {
                 if (i == 0) {
+                    error_pos.* = input.ptr;
                     return error.InvalidName;
                 }
                 try tokens.append(.{ .name = input[0..i] });
@@ -120,33 +160,70 @@ test tokenizeName {
     const gpa = std.testing.allocator;
     var tokens = Tokens.init(gpa);
     defer tokens.deinit();
-    try testing.expectEqualStrings("", try tokenizeName("test", &tokens));
+    var error_pos: ?[*]const u8 = null;
+    try testing.expectEqualStrings(
+        "",
+        try tokenizeName(
+            "test",
+            &tokens,
+            &error_pos,
+        ),
+    );
     try testing.expectEqual(@as(usize, 1), tokens.items.len);
     try testing.expectEqualDeep(Token{ .name = "test" }, tokens.items[0]);
-    try testing.expectEqualStrings(" ", try tokenizeName("test ", &tokens));
+    try testing.expectEqualStrings(
+        " ",
+        try tokenizeName(
+            "test ",
+            &tokens,
+            &error_pos,
+        ),
+    );
     try testing.expectEqual(@as(usize, 2), tokens.items.len);
     try testing.expectEqualDeep(Token{ .name = "test" }, tokens.items[1]);
-    try testing.expectError(error.InvalidName, tokenizeName(": bool", &tokens));
+    const testString = ": bool";
+    try testing.expectError(
+        error.InvalidName,
+        tokenizeName(
+            testString,
+            &tokens,
+            &error_pos,
+        ),
+    );
+    try testing.expectEqual(testString.ptr, error_pos.?);
+    error_pos = null;
     try testing.expectEqual(@as(usize, 2), tokens.items.len);
 }
 
 /// Tokenize a Varlink interface into a .name token and return the rest of the
 /// input.
-fn tokenizeInterface(input: []const u8, tokens: *Tokens) ![]const u8 {
-    const after_interface = skipWord(input, "interface") catch
+fn tokenizeInterface(
+    input: []const u8,
+    tokens: *Tokens,
+    error_pos: *?[*]const u8,
+) ![]const u8 {
+    const after_interface = skipWord(input, "interface", &error_pos.*) catch
         return error.ExpectedInterfaceKeyword;
     const name_start = skipAllWhitespace(after_interface);
-    const eol_start = try tokenizeName(name_start, tokens);
-    return skipEol(eol_start);
+    const eol_start = try tokenizeName(name_start, tokens, error_pos);
+    return skipEol(eol_start, &error_pos.*);
 }
 
 test tokenizeInterface {
     const gpa = std.testing.allocator;
     var tokens = Tokens.init(gpa);
     defer tokens.deinit();
+    var error_pos: ?[*]const u8 = null;
     const test_string = "interface\n org.varlink.service \n test";
     const expected_token: Token = .{ .name = "org.varlink.service" };
-    try testing.expectEqualStrings(" test", try tokenizeInterface(test_string, &tokens));
+    try testing.expectEqualStrings(
+        " test",
+        try tokenizeInterface(
+            test_string,
+            &tokens,
+            &error_pos,
+        ),
+    );
     try testing.expectEqual(@as(usize, 1), tokens.items.len);
     try testing.expectEqualDeep(expected_token, tokens.items[0]);
 }
