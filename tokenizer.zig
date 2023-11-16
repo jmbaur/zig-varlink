jmbaur/zig-varlink

commit 8a659fabae1bbedafab836eb3b2dde11d1d3e991

tokenizer: Fix Unicode line separator skipping

tokenizer.zig
@@ -40,7 +40,7 @@ fn skipLineWhitespace(input: []const u8) struct { bool, []const u8 } {
     }) {
         switch (codepoint) {
             '\n', '\r', '\u{2028}', '\u{2029}' => {
-                return .{ true, input[start + 1 ..] };
+                return .{ true, input[start + std.unicode.utf8CodepointSequenceLength(codepoint) ..] };
             },
             ' ',
             '\t',

commit 67fc1c24cf05923b1d44be172f7df1eb6d204a41

Fix compile error
Fixes: 8a659fa ("tokenizer: Fix Unicode line separator skipping")

tokenizer.zig
@@ -39,8 +39,9 @@ fn skipLineWhitespace(input: []const u8) struct { bool, []const u8 } {
         start += std.unicode.utf8CodepointSequenceLength(codepoint) catch unreachable;
     }) {
         switch (codepoint) {
-            '\n', '\r', '\u{2028}', '\u{2029}' => {
-                return .{ true, input[start + std.unicode.utf8CodepointSequenceLength(codepoint) ..] };
+            '\n', '\r', '\u{2028}', '\u{2029}' => return .{
+                true,
+                input[start + (std.unicode.utf8CodepointSequenceLength(codepoint) catch unreachable) ..],
             },
             ' ',
             '\t',