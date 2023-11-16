jmbaur/zig-varlink

commit 17fe94f48ce76f3bbd530e4a064e580add575db8

handler: Use per-call handlers
This makes handler implementations more readable but may add some
confusion because there's no compile-time feedback for typoed hander
names.

handler.zig
@@ -87,12 +87,24 @@ fn handleMethod(
                 },
                 else => return err,
             };
-            interface_context.handleRequest(
-                input,
-                response_stream,
-                options,
-                extra_data,
-            );
+            const request_function = "handle" ++ decl.name;
+            if (@hasDecl(@TypeOf(interface_context.*), request_function)) {
+                @call(.auto, @field(
+                    @TypeOf(interface_context.*),
+                    request_function,
+                ), .{
+                    interface_context,
+                    input,
+                    response_stream,
+                    options,
+                    extra_data,
+                });
+            } else {
+                serializeResponse(
+                    response_stream,
+                    orgVarlinkService.MethodNotImplemented{ .method = qualified_method },
+                );
+            }
             return;
         }
     }
@@ -211,59 +223,65 @@ pub fn handleRequest(
 fn OrgVarlinkServiceImpl(comptime Context: type) type {
     return struct {
         const interface = orgVarlinkService;
-        fn handleRequest(
+
+        fn handleGetInfo(
             context: *@This(),
-            parameters: anytype,
+            parameters: orgVarlinkService.GetInfo.Parameters,
+            response_stream: anytype,
+            options: Options,
+            extra_data: void,
+        ) void {
+            _ = context;
+            _ = parameters;
+            _ = extra_data;
+            if (options.oneway) {
+                return;
+            }
+            serializeResponse(response_stream, orgVarlinkService.GetInfo.ReturnType{
+                .vendor = Context.vendor,
+                .product = Context.product,
+                .version = Context.version,
+                .url = Context.url,
+                .interfaces = .{"org.varlink.service"} ++ std.meta.fieldNames(Context),
+            }) catch {};
+        }
+
+        fn handleGetInterfaceDescription(
+            context: *@This(),
+            parameters: orgVarlinkService.GetInterfaceDescription.Parameters,
             response_stream: anytype,
             options: Options,
             extra_data: void,
         ) void {
             _ = context;
             _ = extra_data;
-            switch (@TypeOf(parameters)) {
-                orgVarlinkService.GetInfo.Parameters => {
-                    if (options.oneway) {
-                        return;
-                    }
-                    serializeResponse(response_stream, orgVarlinkService.GetInfo.ReturnType{
-                        .vendor = Context.vendor,
-                        .product = Context.product,
-                        .version = Context.version,
-                        .url = Context.url,
-                        .interfaces = .{"org.varlink.service"} ++ std.meta.fieldNames(Context),
-                    }) catch {};
-                },
-                orgVarlinkService.GetInterfaceDescription.Parameters => {
-                    if (options.oneway) {
-                        return;
-                    }
-                    inline for (@typeInfo(Context).Struct.fields) |field| {
-                        if (std.mem.eql(u8, parameters.interface, field.name)) {
-                            serializeResponse(
-                                response_stream,
-                                orgVarlinkService.GetInterfaceDescription.ReturnType{
-                                    .description = field.type.interface.description,
-                                },
-                            ) catch {};
-                            return;
-                        }
-                    }
-                    if (std.mem.eql(u8, "org.varlink.service", parameters.interface)) {
-                        serializeResponse(
-                            response_stream,
-                            orgVarlinkService.GetInterfaceDescription.ReturnType{
-                                .description = orgVarlinkService.description,
-                            },
-                        ) catch {};
-                        return;
-                    }
+            if (options.oneway) {
+                return;
+            }
+            inline for (@typeInfo(Context).Struct.fields) |field| {
+                if (std.mem.eql(u8, parameters.interface, field.name)) {
                     serializeResponse(
                         response_stream,
-                        orgVarlinkService.InterfaceNotFound{ .interface = parameters.interface },
+                        orgVarlinkService.GetInterfaceDescription.ReturnType{
+                            .description = field.type.interface.description,
+                        },
                     ) catch {};
-                },
-                else => @compileError("Unhandled org.varlink.service request!"),
+                    return;
+                }
             }
+            if (std.mem.eql(u8, "org.varlink.service", parameters.interface)) {
+                serializeResponse(
+                    response_stream,
+                    orgVarlinkService.GetInterfaceDescription.ReturnType{
+                        .description = orgVarlinkService.description,
+                    },
+                ) catch {};
+                return;
+            }
+            serializeResponse(
+                response_stream,
+                orgVarlinkService.InterfaceNotFound{ .interface = parameters.interface },
+            ) catch {};
         }
     };
 }

test/handler-test.zig
@@ -14,25 +14,20 @@ const Context = struct {
     @"org.zig-varlink.test": struct {
         pub const interface = zigVarlinkTest;
         counter: u32 = 0,
-        pub fn handleRequest(
+        pub fn handleTestCall(
             context: *@This(),
-            parameters: anytype,
+            parameters: zigVarlinkTest.TestCall.Parameters,
             response_stream: anytype,
             options: handler.Options,
             extra_info: u32,
         ) void {
-            switch (@TypeOf(parameters)) {
-                zigVarlinkTest.TestCall.Parameters => {
-                    context.counter += 1;
-                    if (options.oneway) {
-                        return;
-                    }
-                    handler.serializeResponse(response_stream, zigVarlinkTest.TestCall.ReturnType{
-                        .out = parameters.in + context.counter + extra_info,
-                    }) catch {};
-                },
-                else => @compileLog(parameters),
+            context.counter += 1;
+            if (options.oneway) {
+                return;
             }
+            handler.serializeResponse(response_stream, zigVarlinkTest.TestCall.ReturnType{
+                .out = parameters.in + context.counter + extra_info,
+            }) catch {};
         }
     } = .{},
 };

commit 125ea94ae7665b7a1022cfc255c5b643effba050

handler: Add a missing try
Fixes: 17fe94f ("handler: Use per-call handlers")

handler.zig
@@ -100,7 +100,7 @@ fn handleMethod(
                     extra_data,
                 });
             } else {
-                serializeResponse(
+                try serializeResponse(
                     response_stream,
                     orgVarlinkService.MethodNotImplemented{ .method = qualified_method },
                 );