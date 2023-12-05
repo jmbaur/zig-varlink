fb813b2b2c3ccf61ca119d643191fcda15a5db36
Väinö Mäkelä <vaino.makela@iki.fi>
2023-12-05 17:50:01 +0200

    varlink.zig: Add copyright header

--- src/varlink.zig
+++ src/varlink.zig
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Väinö Mäkelä <vaino.makela@iki.fi>
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 pub const json = @import("json.zig");
 pub const Options = @import("router.zig").Options;
 pub const Client = @import("client.zig").Client;
