5a0a99c2a93f642ca7728d9fb1718465acbf2856
Väinö Mäkelä <vaino.makela@iki.fi>
2023-12-05 18:08:27 +0200

    varlink: Make orgVarlinkService public
    This allows library users to use the Varlink service's errors.

--- src/varlink.zig
+++ src/varlink.zig
@@ -6,3 +6,4 @@ pub const json = @import("json.zig");
 pub const Options = @import("router.zig").Options;
 pub const Client = @import("client.zig").Client;
 pub const server = @import("server.zig");
+pub const service = @import("orgVarlinkService");
